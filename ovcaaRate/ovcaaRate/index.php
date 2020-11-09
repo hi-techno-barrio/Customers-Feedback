fc6ca733e7c975a751e09e126d1fd754908b1b8e
Christopher Coballes <cobecoballes@gmail.com>
2020-11-09 14:54:30 +0800

    Update index.php

--- ovcaaRate/ovcaaRate/index.php
+++ ovcaaRate/ovcaaRate/index.php
@@ -56,7 +56,7 @@
 <form method="post" action="insert_rating.php">
   <p id="total_votes">Total Ratings:<?php echo $total;?></p>
   <div class="div">
-	  <p>Quality Service (<?php echo $total_php_rating;?>)</p>
+	  <p>Service Quality (<?php echo $total_php_rating;?>)</p>
 	  <input type="hidden" id="php1_hidden" value="1">
 	  <img src="images/star1.png" onmouseover="change(this.id);" id="php1" class="php">
 	  <input type="hidden" id="php2_hidden" value="2">
@@ -70,7 +70,7 @@
   </div>
 
   <div class="div">
-	  <p>Quick Service (<?php echo $total_asp_rating;?>)</p>
+	  <p>Service Efficiency (<?php echo $total_asp_rating;?>)</p>
 	  <input type="hidden" id="asp1_hidden" value="1">
 	  <img src="images/star1.png" onmouseover="change(this.id);" id="asp1" class="asp">
 	  <input type="hidden" id="asp2_hidden" value="2">
@@ -84,7 +84,7 @@
   </div>
 
   <div class="div">
-  	  <p>Quasi Service (<?php echo $total_jsp_rating;?>)</p>
+  	  <p>Service Timeliness (<?php echo $total_jsp_rating;?>)</p>
 	  <input type="hidden" id="jsp1_hidden" value="1">
 	  <img src="images/star1.png" onmouseover="change(this.id);" id="jsp1" class="jsp">
 	  <input type="hidden" id="jsp2_hidden" value="2">
@@ -97,6 +97,20 @@
 	  <img src="images/star1.png" onmouseover="change(this.id);" id="jsp5" class="jsp">
   </div>
 
+
+  <div class="div">
+  	  <p>Service Attitude (<?php echo $total_jsp_rating;?>)</p>
+	  <input type="hidden" id="jsp1_hidden" value="1">
+	  <img src="images/star1.png" onmouseover="change(this.id);" id="jsp1" class="jsp">
+	  <input type="hidden" id="jsp2_hidden" value="2">
+	  <img src="images/star1.png" onmouseover="change(this.id);" id="jsp2" class="jsp">
+	  <input type="hidden" id="jsp3_hidden" value="3">
+	  <img src="images/star1.png" onmouseover="change(this.id);" id="jsp3" class="jsp">
+	  <input type="hidden" id="jsp4_hidden" value="4">
+	  <img src="images/star1.png" onmouseover="change(this.id);" id="jsp4" class="jsp">
+	  <input type="hidden" id="jsp5_hidden" value="5">
+	  <img src="images/star1.png" onmouseover="change(this.id);" id="jsp5" class="jsp">
+  </div>
   <input type="hidden" name="phprating" id="phprating" value="0">
   <input type="hidden" name="asprating" id="asprating" value="0">
   <input type="hidden" name="jsprating" id="jsprating" value="0">
